96c23446102fe57d6fd5231ce3c31abb97245855
Seasoned Soda <kals0415@gmail.com>
2026-06-15 13:52:03 -0700

    Starting

---
+++ List.js
@@ -0,0 +1,52 @@
+const restaurauntList = [
+{1 : 'Cowami'},
+{2 :  'Pollo loco'}, 
+{3 : 'Norms'} ,
+{4 : 'Panera'} ,
+{5 : 'Mexican restaurant on Azusa'},
+{6 : "pepe's"},
+{7 : 'farmer boys'}, 
+{8 : 'hat'},
+{9 : 'karas'},
+{10 : 'old wold deli'} ,
+{11 : 'Marie callenders'},
+{12 : 'Mongolian BBQ '},
+{13 : 'El torito'} ,
+{14 : 'islands'} ,
+{15 : 'habit'} ,
+{16 : 'in-N-Out'} ,
+{17 : 'baby Bros'} ,
+{18 : 'the boat'},
+{19 : 'lazy dog'} ,
+{20 : 'Red Robin'},
+{21 : 'Applbee'},
+{22 : 'Hawaiian BBQ'},
+{23 : 'barbecue'},
+{24 : 'sizzler'} ,
+{25 : 'pho'},
+{26 : 'bj'},
+{27 : 'chiles'},
+{28 : 'Freddys'},
+{29 : 'jimtsy  Mexican restaurant'} ,
+{30 : 'town sen china'} ,
+{31 : 'cj fresh food'} ,
+{32 : 'bun (by the West Covina theater)'},
+{33 : 'canyon City BBQ'} ,
+{34 : 'Dennys'} ,
+{35 : 'flows'} ,
+{36 : 'chopsticks San dimas.' },
+{37 : 'lolas'},
+{38 : 'chubby Curry'} ,
+{39 : 'perry joint (with Eleanor'},
+{40 : 'pho place by glendora shell'},
+{41 : 'steakhouse outback' },
+{42 : 'Pinnacle peaks.'}
+]
+function randomNumber(){
+    const number = Math.random() * restaurauntList.length
+    console.log(Math.round(number))
+}
+
+function restaurant(){
+    
+}
