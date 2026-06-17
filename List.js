5905ab4c349ddf16e1e49e5339ac0c4866b4662c
Seasoned Soda <kals0415@gmail.com>
2026-06-17 15:56:38 -0700

    centered content, clicking button adds new div with restaurant picked

--- List.js
+++ List.js
@@ -1,52 +1,62 @@
 const restaurauntList = [
-{1 : 'Cowami'},
-{2 :  'Pollo loco'}, 
-{3 : 'Norms'} ,
-{4 : 'Panera'} ,
-{5 : 'Mexican restaurant on Azusa'},
-{6 : "pepe's"},
-{7 : 'farmer boys'}, 
-{8 : 'hat'},
-{9 : 'karas'},
-{10 : 'old wold deli'} ,
-{11 : 'Marie callenders'},
-{12 : 'Mongolian BBQ '},
-{13 : 'El torito'} ,
-{14 : 'islands'} ,
-{15 : 'habit'} ,
-{16 : 'in-N-Out'} ,
-{17 : 'baby Bros'} ,
-{18 : 'the boat'},
-{19 : 'lazy dog'} ,
-{20 : 'Red Robin'},
-{21 : 'Applbee'},
-{22 : 'Hawaiian BBQ'},
-{23 : 'barbecue'},
-{24 : 'sizzler'} ,
-{25 : 'pho'},
-{26 : 'bj'},
-{27 : 'chiles'},
-{28 : 'Freddys'},
-{29 : 'jimtsy  Mexican restaurant'} ,
-{30 : 'town sen china'} ,
-{31 : 'cj fresh food'} ,
-{32 : 'bun (by the West Covina theater)'},
-{33 : 'canyon City BBQ'} ,
-{34 : 'Dennys'} ,
-{35 : 'flows'} ,
-{36 : 'chopsticks San dimas.' },
-{37 : 'lolas'},
-{38 : 'chubby Curry'} ,
-{39 : 'perry joint (with Eleanor'},
-{40 : 'pho place by glendora shell'},
-{41 : 'steakhouse outback' },
-{42 : 'Pinnacle peaks.'}
+{restaurant : 'Cowami'},
+{restaurant :  'Pollo loco'}, 
+{restaurant : 'Norms'} ,
+{restaurant : 'Panera'} ,
+{restaurant : 'Mexican restaurant on Azusa'},
+{restaurant : "pepe's"},
+{restaurant : 'farmer boys'}, 
+{restaurant : 'hat'},
+{restaurant : 'karas'},
+{restaurant : 'old wold deli'} ,
+{restaurant : 'Marie callenders'},
+{restaurant : 'Mongolian BBQ '},
+{restaurant : 'El torito'} ,
+{restaurant : 'islands'} ,
+{restaurant : 'habit'} ,
+{restaurant : 'in-N-Out'} ,
+{restaurant : 'baby Bros'} ,
+{restaurant : 'the boat'},
+{restaurant : 'lazy dog'} ,
+{restaurant : 'Red Robin'},
+{restaurant : 'Applbee'},
+{restaurant : 'Hawaiian BBQ'},
+{restaurant : 'barbecue'},
+{restaurant : 'sizzler'} ,
+{restaurant : 'pho'},
+{restaurant : 'bj'},
+{restaurant : 'chiles'},
+{restaurant : 'Freddys'},
+{restaurant : 'jimtsy  Mexican restaurant'} ,
+{restaurant : 'town sen china'} ,
+{restaurant : 'cj fresh food'} ,
+{restaurant : 'bun (by the West Covina theater)'},
+{restaurant : 'canyon City BBQ'} ,
+{restaurant : 'Dennys'} ,
+{restaurant : 'flows'} ,
+{restaurant : 'chopsticks San dimas.' },
+{restaurant : 'lolas'},
+{restaurant : 'chubby Curry'} ,
+{restaurant : 'perry joint (with Eleanor'},
+{restaurant : 'pho place by glendora shell'},
+{restaurant : 'steakhouse outback' },
+{restaurant : 'Pinnacle peaks.'}
 ]
 function randomNumber(){
     const number = Math.random() * restaurauntList.length
-    console.log(Math.round(number))
+    return Math.round(number)
 }
 
-function restaurant(){
+const form = document.querySelector('.randomize')
+const namePlacement = document.querySelector('.restaurantName')
+
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const restaurantPicked = restaurauntList[randomNumber()]
+    const newDiv = document.createElement('DIV')
+    newDiv.append(restaurantPicked.restaurant)
+    namePlacement.append(newDiv)
     
-}
+})
